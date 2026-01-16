3731398ef55064afc9b66addf0cc1da3f8998647
Thu Hang Vu <11247393@st.neu.edu.vn>
2026-01-16 19:08:41 +0700

    required

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const { connectDB } = require('./config/db');
-const routes = require('./routes');
-const { logger, notFound, errorHandler } = require('./middlewares');
+const { connectDB } = require('../config/db');
+const routes = require('../routes');
+const { logger, notFound, errorHandler } = require('../middlewares');
 
 const app = express();
 
